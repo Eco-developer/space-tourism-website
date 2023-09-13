453457607fd6ca0308e9e7eed040ebf71a248c6f
Jose Quesada <josesk8mqc@gmail.com>
2023-09-12 20:30:34 -0400

    create template of Technology page with its module.css,

---
+++ src/pages/Technology/Technology.jsx
@@ -0,0 +1,11 @@
+import styles from "./Technology.module.css";
+
+const Technology = () => {
+    return (
+        <div className={styles.technology__container}>
+            tecnology
+        </div>
+    )
+}
+
+export default Technology;
