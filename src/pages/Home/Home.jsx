29cb2d6c9679a68ecf8c76710b62535d9585a049
Jose Quesada <josesk8mqc@gmail.com>
2023-09-12 20:29:21 -0400

    create template of Home page with its module.css,

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import styles from "./Home.module.css";
+
+const Home = () => {
+    return (
+        <div className={styles.home__container}>
+           Home
+        </div>
+    )
+}
+
+export default Home;
