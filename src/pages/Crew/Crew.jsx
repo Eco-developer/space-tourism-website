fddc85f0b82238464c585fce87a9790c9a798399
Jose Quesada <josesk8mqc@gmail.com>
2023-09-12 20:30:16 -0400

    create template of Crew page with its module.css,

---
+++ src/pages/Crew/Crew.jsx
@@ -0,0 +1,11 @@
+import styles from "./Crew.module.css";
+
+const Crew = () => {
+    return (
+        <div className={styles.crew__container}>
+           Crew
+        </div>
+    )
+}
+
+export default Crew;
