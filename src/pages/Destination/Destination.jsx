a8236aa841eaddbb6418def33517ec170819f605
Jose Quesada <josesk8mqc@gmail.com>
2023-09-12 20:29:38 -0400

    create template of Destination page with its module.css,

---
+++ src/pages/Destination/Destination.jsx
@@ -0,0 +1,11 @@
+import styles from "./Destination.module.css";
+
+const Destination = () => {
+    return (
+        <div className={styles.destination__container}>
+           Destination
+        </div>
+    )
+}
+
+export default Destination;
