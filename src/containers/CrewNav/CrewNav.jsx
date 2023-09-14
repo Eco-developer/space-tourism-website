168677a78aa8857627ba3793396caf7502bb2397
Jose Quesada <josesk8mqc@gmail.com>
2023-09-14 12:18:12 -0400

    delete logic of interval to move to custom hook useInterval.

--- src/containers/CrewNav/CrewNav.jsx
+++ src/containers/CrewNav/CrewNav.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef } from "react";
 import styles from "./CrewNav.module.css";
 import { v4 as uuid } from "uuid"
 
@@ -6,40 +5,22 @@ export const CrewNav = ({
     data,
     position,
     setPosition
-}) => {
-    const didMount = useRef(false);
-    useEffect(() => {
-        if (didMount.current) {
-            return
-        }
-        setInterval(() => {
-            setPosition((prevPosition) => {
-                if (prevPosition === data.length - 1) {
-                    return 0;
-                } else {
-                    return prevPosition+1;
+}) => (
+    <div className={styles.crew_nav__container}>
+        <div  className={styles.crew_nav__content}>    
+            {data.map((item, index) => {
+                const handleClick = () => {
+                    setPosition(index)
                 }
-            })
-        }, 3000)
-        didMount.current = true
-    }, [data, setPosition])
-    return (
-        <div className={styles.crew_nav__container}>
-            <div  className={styles.crew_nav__content}>    
-                {data.map((item, index) => {
-                    const handleClick = () => {
-                        setPosition(index)
-                    }
-                    return (
-                        <div 
-                            className={`${position === index ? styles.active_item : ""} ${styles.nav_dot}`}
-                            key={uuid()}
-                            onClick={handleClick}
-                        >
-                        </div>  
-                    )
-                })}
-            </div>
+                return (
+                    <div 
+                        className={`${position === index ? styles.active_item : ""} ${styles.nav_dot}`}
+                        key={uuid()}
+                        onClick={handleClick}
+                    >
+                    </div>  
+                )
+            })}
         </div>
-    ) 
-}
+    </div>
+) 
